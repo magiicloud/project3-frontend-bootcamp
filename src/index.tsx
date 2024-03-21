02aed134def66c51d2533a81b49826b5dd8ef4f0
magiicloud <hongyun_rc@outlook.com>
2024-03-21 18:13:01 +0800

    converted proj into TS

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN!}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID!}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);

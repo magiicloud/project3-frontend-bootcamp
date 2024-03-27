8b620713bafbd952add08df9d28692c86c00e815
magiicloud <hongyun_rc@outlook.com>
2024-03-27 08:57:12 +0800

    amended dependencies version, changed module to ESNext, retrived items from backend

--- src/pages/AllItems.tsx
+++ src/pages/AllItems.tsx
@@ -1,12 +1,62 @@
-import React, { useEffect } from "react";
-import { cn } from "../lib/utils";
-import { buttonVariants } from "../components/ui/button";
+import React, { useEffect, useState } from "react";
+import { BACKEND_URL } from "../constants";
+import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+
+interface Item {
+  id: number;
+  serial_num: string;
+  item_name: string;
+  par_level: number;
+}
 
 export const AllItems = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const allItemsData = await axios.get(`${BACKEND_URL}/allitems/`);
+        setItems(allItemsData.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getItems();
+  }, []);
   return (
     <>
-      <div className="prose">
-        <h1>All Items</h1>
+      <div className="prose grid grid-cols-1 p-6 max-w-full sm:grid-cols-6 sm:p-12">
+        <h1 className="text-left p-2 text-gray-600 sm:col-span-6">All Items</h1>
+        <div className="sm:col-span-6">
+          <Table>
+            <TableCaption>A list of all items.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Serial No.</TableHead>
+                <TableHead>Item Name</TableHead>
+                <TableHead>Par Level</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {items.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.serial_num}</TableCell>
+                  <TableCell>{item.item_name}</TableCell>
+                  <TableCell>{item.par_level}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </>
   );

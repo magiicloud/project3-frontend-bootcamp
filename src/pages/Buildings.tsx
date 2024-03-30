4201a3f7d96ef7ecc95d3f053c7d24fa88209216
patrickkok <pkok1996@gmail.com>
2024-03-30 20:16:28 +0800

    Add feature to fetch and display current buildings from the backend

--- src/pages/Buildings.tsx
+++ src/pages/Buildings.tsx
@@ -2,13 +2,15 @@ import React, { useEffect } from "react";
 import { cn } from "../lib/utils";
 import { buttonVariants } from "../components/ui/button";
 import { NewBuilding } from "../components/NewBuilding";
+import { BuildingsList } from "../components/BuildingsList";
 
 export const Buildings = () => {
   return (
     <>
-      <div className="prose">
+      <div className="prose max-w-none">
         <h1>BUILDINGS</h1>
         <NewBuilding />
+        <BuildingsList />
       </div>
     </>
   );

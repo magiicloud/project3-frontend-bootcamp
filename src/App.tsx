bd61a1445b3d7a8a3331b65c5e78675905dd1bfc
magiicloud <hongyun_rc@outlook.com>
2024-04-01 23:42:33 +0800

    added protected routes and auth but not working

--- src/App.tsx
+++ src/App.tsx
@@ -25,3 +25,42 @@ const App = () => {
 };
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Login } from "./pages/Login";
+// import { Dashboard } from "./pages/Dashboard";
+// import { Buildings } from "./pages/Buildings";
+// import { Room } from "./pages/Room";
+// import { AllItems } from "./pages/AllItems";
+// import { ManageItems } from "./pages/ManageItems";
+// import { AddNewItem } from "./pages/AddNewItem";
+// import { ProtectedRoute } from "./components/ProtectedRoutes";
+
+// const protectedRoutes = [
+//   { path: "/dashboard", element: <Dashboard /> },
+//   { path: "/buildings", element: <Buildings /> },
+//   { path: "/room", element: <Room /> },
+//   { path: "/allitems", element: <AllItems /> },
+//   { path: "/manageitems", element: <ManageItems /> },
+//   { path: "/addnewitem", element: <AddNewItem /> },
+// ];
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         {protectedRoutes.map(({ path, element }) => (
+//           <Route
+//             key={path}
+//             path={path}
+//             element={<ProtectedRoute>{element}</ProtectedRoute>}
+//           />
+//         ))}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;

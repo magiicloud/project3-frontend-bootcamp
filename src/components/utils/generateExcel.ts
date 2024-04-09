9e69c83009f3a2ad19acd83bfb2e988d407d22b7
magiicloud <hongyun_rc@outlook.com>
2024-04-09 09:32:44 +0800

    added download excel function for all items table to download selected rows

--- src/components/utils/generateExcel.ts
+++ src/components/utils/generateExcel.ts
@@ -21,6 +21,17 @@ interface ParItem {
   serial_num: string;
 }
 
+export interface AllItemsTable {
+  id: number;
+  serial_num: string;
+  item_name: string;
+  par_level: number;
+  room: string;
+  quantity: number;
+  uom: string;
+  expiry_date: string;
+}
+
 export const generateExpItemExcel = async (expItems: ExpItem[]) => {
   const workbook = new ExcelJS.Workbook();
   const worksheet = workbook.addWorksheet("ExpItems");
@@ -95,6 +106,52 @@ export const generateParItemExcel = async (expItems: ParItem[]) => {
   const anchor = document.createElement("a");
   anchor.href = url;
   anchor.download = "ParItems.xlsx"; // Name of the file to be downloaded
+  document.body.appendChild(anchor); // Required for Firefox
+  anchor.click();
+
+  // Clean up
+  document.body.removeChild(anchor);
+  URL.revokeObjectURL(url);
+};
+
+export const generateAllItemsTableExcel = async (allItems: any[]) => {
+  console.log(allItems);
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("AllItems");
+
+  worksheet.columns = [
+    { header: "Serial No.", key: "serial_num", width: 20 },
+    { header: "Item Name", key: "item_name", width: 30 },
+    { header: "Par Level", key: "par_level", width: 15 },
+    { header: "Room", key: "room", width: 15 },
+    { header: "Quantity", key: "quantity", width: 10 },
+    { header: "UOM", key: "uom", width: 10 },
+    { header: "Exp Date", key: "expiry_date", width: 15 },
+  ];
+
+  allItems.forEach((item) => {
+    worksheet.addRow({
+      serial_num: item.serial_num,
+      item_name: item.item_name,
+      par_level: item.par_level,
+      room: item.room,
+      quantity: Number(item.quantity),
+      uom: item.uom,
+      expiry_date: item.expiry_date,
+    });
+  });
+
+  // Use a Blob to generate a file in-memory and trigger a download
+  const buffer = await workbook.xlsx.writeBuffer();
+  const blob = new Blob([buffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+  const url = URL.createObjectURL(blob);
+
+  // Create a temporary anchor to trigger download
+  const anchor = document.createElement("a");
+  anchor.href = url;
+  anchor.download = "AllItems.xlsx"; // Name of the file to be downloaded
   document.body.appendChild(anchor); // Required for Firefox
   anchor.click();
 
